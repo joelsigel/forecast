44314a4181d0cfb5ce158003d333b8e5c4fcc486
“Joel <joelsigel@gmail.com>
2018-05-10 13:52:21 -0700

    R&D - Testing

--- controller/app.js
+++ controller/app.js
@@ -39,22 +39,33 @@ class App {
 			data = json[i];
 
 			// data sets
-			var avail = (data.available ? 'available' : 'not-available');
+			var avail = (data.available ? 'enabled' : 'disabled');
 
 			// test iteration of array
 			console.log(i);
 
 			// set content
 			content = `
-			<div class="col-xs-6">
+			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-6">
 				<div class="panel panel-primary">
 				  <div class="panel-heading">
-						<span class="title">`+avail+`</span>
-				    <span class="options-menu"><i class="fal fa-sliders-h fa-2x"></i></span>
+						<div class="row">
+							<div class="col-xs-10">
+								<span class="title">`+avail+`</span>
+							</div>
+							<div class="col-xs-2">
+				    		<span class="options-menu"><i class="fal fa-sliders-h"></i></span>
+							</div>
+						</div>
 				 </div>
 				 <div class="panel-body">
 				   <div class="row">
-
+					 	<div class="col-xs-4">
+							<h5>May<br><small>2018</small></h5>
+						</div>
+						<div class="col-xs-4">
+							<h1>$2,345.32</h1>
+						</div>
 					 </div>
 				 </div>
 			  </div>
