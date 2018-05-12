8bf2ff6fb89328fe307fee9987a7a9f0108fb89e
“Joel <joelsigel@gmail.com>
2018-05-11 22:40:33 -0700

    1. Data pull from google sheets 2. Line graph dynamic based on total due 3. Styling

--- controller/app.js
+++ controller/app.js
@@ -22,12 +22,13 @@ class App {
 		var storage = new Storage(), app = new App();
 		$.getJSON('data/data.json', function(json) {
 			if (Object.keys(json).length < 1) {
-				console.log('json data empty');
+				// console.log('json data empty');
 			}
 			else {
-				console.log('json data loaded');
+				// console.log('json data loaded');
 				storage.dataRead(json);
 				app.loadData(json);
+				app.loadUtilities(json);
 			}
 		});
 	}
@@ -40,14 +41,14 @@ class App {
 
 			// data sets
 			var avail = (data.available ? 'enabled' : 'disabled');
-			var institute = (data.available && data.bank ? data.bank.name : '')
+			var institute = (data.available && data.bank ? data.bank.name : '');
 
 			// test iteration of array
-			console.log(i);
+			// console.log(i);
 
 			// set content
 			content = `
-			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-6">
+			<div class="col-xs-offset-3 col-xs-6">
 				<div class="panel panel-primary">
 				  <div class="panel-heading">
 						<div class="row">
@@ -64,79 +65,17 @@ class App {
 						</div>
 				 </div>
 				 <div class="panel-body">
-				   <div class="row">
-					 	<div class="col-xs-4">
+			   	<div class="row">
+				 		<div class="col-xs-4">
 							<h5>May<br><small>2018</small></h5>
 						</div>
 						<div class="col-xs-8">
-							<h1>$2,345.32</h1>
+							<h1 class="current"><?php echo $g_data['total-amount']; ?></h1>
 						</div>
-					 </div>
-					 <div class="row">
-					 	<div class="col-xs-4">
-							<div class="utilities auto">
-								<span class="line"></span>
-								<p>Auto</p>
-								<h5>$220.00</h5>
-								<span class="pull-right">
-									<i class="fas fa-car"></i>
-								</span>
-							</div>
-						</div>
-						<div class="col-xs-4">
-							<div class="utilities electrical">
-								<span class="line"></span>
-								<p>Electrical</p>
-								<h5>$120.56</h5>
-								<span class="pull-right">
-									<i class="fas fa-bolt"></i>
-								</span>
-							</div>
-						</div>
-						<div class="col-xs-4">
-							<div class="utilities internet">
-								<span class="line"></span>
-								<p>Internet</p>
-								<h5>$158.73</h5>
-								<span class="pull-right">
-									<i class="fas fa-wifi"></i>
-								</span>
-							</div>
-						</div>
-					 </div>
-					 <div class="row">
-					 	<div class="col-xs-4">
-							<div class="utilities phone">
-								<span class="line"></span>
-								<p>Phone</p>
-								<h5>$180.00</h5>
-								<span class="pull-right">
-									<i class="fas fa-phone"></i>
-								</span>
-							</div>
-						</div>
-						<div class="col-xs-4">
-							<div class="utilities insurance">
-								<span class="line"></span>
-								<p>Insurance</p>
-								<h5>$115.84</h5>
-								<span class="pull-right">
-									<i class="fas fa-shield"></i>
-								</span>
-							</div>
-						</div>
-						<div class="col-xs-4">
-							<div class="utilities mortage">
-								<span class="line"></span>
-								<p>Mortage</p>
-								<h5>$1158.56</h5>
-								<span class="pull-right">
-									<i class="fas fa-home"></i>
-								</span>
-							</div>
-						</div>
-					 </div>
-				 </div>
+				 	</div>
+					<section class="utility-drop"></section>
+				</div>
+
 				 <div class="panel-footer">
 			 	 	<div class="row" align="center">
 						<div class="col-xs-3">
@@ -172,6 +111,100 @@ class App {
 
 
 		}
+		$('.current').load("pull/data-pull.php").fadeIn();
 	}
+
+
+	// load utilities
+	loadUtilities(json) {
+		for (var i=0; i < Object.keys(json).length; i++) {
+			var data, rowstart, content, rowend;
+			data = json[i];
+
+			//
+			var mortgage = (data.available && data.utilities ? data.utilities.mortgage.name : "");
+			var mortgagecost = (data.available && data.utilities ? data.utilities.mortgage.amount : "");
+
+			if (i % 3 === 0) {
+				rowstart =`<div class="row">`;
+			}
+			else {
+				rowstart=``;
+			}
+
+			content = `
+			<div class="col-xs-4">
+			  <div class="utilities `+mortgage+`">
+			    <span class="line"></span>
+			    <p>`+mortgage+`</p>
+			    <h5 class="cost">`+mortgagecost+`</h5>
+			    <span class="pull-right">
+			      <i class="fas fa-home"></i>
+			    </span>
+			  </div>
+			</div>
+			<div class="col-xs-4">
+			  <div class="utilities phone">
+			    <span class="line"></span>
+			    <p>Phone</p>
+			    <h5 class="cost">180.00</h5>
+			    <span class="pull-right">
+			      <i class="fas fa-phone"></i>
+			    </span>
+			  </div>
+			</div>
+			<div class="col-xs-4">
+			  <div class="utilities insurance">
+			    <span class="line"></span>
+			    <p>Insurance</p>
+			    <h5 class="cost">115.84</h5>
+			    <span class="pull-right">
+			      <i class="fas fa-shield"></i>
+			    </span>
+			  </div>
+			</div>
+			<div class="col-xs-4">
+			  <div class="utilities auto">
+			    <span class="line"></span>
+			    <p>Auto</p>
+			    <h5 class="cost">220.00</h5>
+			    <span class="pull-right">
+			      <i class="fas fa-car"></i>
+			    </span>
+			  </div>
+			</div>
+			<div class="col-xs-4">
+			  <div class="utilities electrical">
+			    <span class="line"></span>
+			    <p>Electrical</p>
+			    <h5 class="cost">120.56</h5>
+			    <span class="pull-right">
+			      <i class="fas fa-bolt"></i>
+			    </span>
+			  </div>
+			</div>
+			<div class="col-xs-4">
+			  <div class="utilities internet">
+			    <span class="line"></span>
+			    <p>Internet</p>
+			    <h5 class="cost">158.73</h5>
+			    <span class="pull-right">
+			      <i class="fas fa-wifi"></i>
+			    </span>
+			  </div>
+			</div>
+			`;
+
+			if (i % 3 === 0) {
+				rowend =`</div>`;
+			}
+			else {
+				rowend=``;
+			}
+
+			$('.utility-drop').append(rowstart + content + rowend);
+		}
+	}
+
 
 }
